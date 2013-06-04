7f35dbd2a9932a2de7b4f9d170f232f20f0ddf8b
Edward Banner <edward.banner@gmail.com>
2013-06-03 22:16:22 -0400

    Modified the dvorak layout some.
    Shifted 6 keys on the top row to find a better place for l (ell) in an attempt
    to save my right pinky.
    Switched Caps Lock and Escape (for vim winnage).
    Swapped i and u (because August would have wanted it that way).

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@
 
 dotfiles=dotfiles            # Dotfiles directory
 old_dir=.dotfiles.old        # Old dotfiles backup directory
-files='bashrc vimrc Xresources dwm-statusbar.sh gitconfig inputrc xinitrc fehbg'
+files='bashrc vimrc Xresources dwm-statusbar.sh gitconfig inputrc xinitrc fehbg Xmodmap'
 
 # Create dotfiles.old in homedir
 echo "Creating $old_dir for backup of any existing dotfiles in $HOME"
