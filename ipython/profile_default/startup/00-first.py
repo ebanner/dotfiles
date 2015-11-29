edb79cf066d9ffba65f915ba32de9e46232648e2
Edward Banner <edward.banner@gmail.com>
2015-11-28 19:49:04 -0600

    Add ipython startup script

---
+++ ipython/profile_default/startup/00-first.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import logging
+from logging import warning as warn
+
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+# Some magic so that the notebook will reload external python modules
+# %load_ext autoreload
+# %autoreload 2
