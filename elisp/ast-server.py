e0c45bfdaea341de701607e3e7d66752290d07ac
Edward Banner <edward.banner@gmail.com>
2017-12-16 20:55:37 -0800

    Add AST server

---
+++ elisp/ast-server.py
@@ -0,0 +1,19 @@
+import ast
+import astor
+from epc.server import EPCServer
+
+server = EPCServer(('localhost', 0))
+
+@server.register_function
+def annotate(*code):
+    """Annotate code with code to make and eval cells"""
+    code = code[0]
+    tree = ast.parse(code)
+    stmts = [astor.to_source(stmt).rstrip() for stmt in tree.body]
+    cell_commands = [f'epc_client.call_sync("""make-code-cell-and-eval""", ["""{stmt}"""])' for stmt in stmts]
+    commands = [f'{stmt}; {cell_command}' for stmt, cell_command in zip(stmts, cell_commands)]
+    new_code = '\n'.join(commands)
+    return new_code
+
+server.print_port()
+server.serve_forever()
