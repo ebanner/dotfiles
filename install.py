c373b23b0a9ee6f67db3b651b4109145cec94389
Edward Banner <edward@Edwards-MacBook-Air.local>
2025-07-06 16:16:23 -0400

    Use stow instead

--- install.py
+++ install.py
@@ -1,5 +1,3 @@
-# Script to symlink all dotfiles. This directory must be at ~/.dotfiles!
-
 import os
 import subprocess
 import tempfile
@@ -19,39 +17,16 @@ def cd(dir):
         print('Could not cd into {0}. Exiting.'.format(dir))
         exit(1)
 
-def read_dotfiles(dotfiles_path):
-    with open(dotfiles_path) as f:
-        dotfiles = [line.strip() for line in f.readlines()]
-    return dotfiles
-
 
 if __name__ == '__main__':
-    """Installs dotfiles to home directory
-
-    Dotfiles already present are backed up then all dotfiles are symlinked from
-    dotfiles/ into the user's home directory. Pathogen and other vim plugins are
-    also installed.
-
-    """
+    """Install pathogen and other vim plugins."""
     cd(HOME)
-    dotfiles = read_dotfiles(DOTFILES_DIR+'dotfiles')
-    tmpdir = tempfile.mkdtemp()
-    for dotfile in dotfiles:
-        try:
-            os.rename('.'+dotfile, tmpdir+dotfile)
-            logging.info('Moved {} to {}'.format(dotfile, tmpdir))
-        except:
-            pass
-        try:
-            os.symlink(DOTFILES_DIR+dotfile, '.'+dotfile)
-            logging.info('Linked {} to {}'.format(DOTFILES_DIR+dotfile, '.'+dotfile))
-        except:
-            pass
-    # vim
+
     try:
         os.makedirs(HOME+'/.vim/autoload')
     except:
         pass
+
     dest, url = HOME+'/.vim/autoload/pathogen.vim', 'https://tpo.pe/pathogen.vim'
     subprocess.call(['curl', '-LSso', dest, url])
     cd('.vim')
