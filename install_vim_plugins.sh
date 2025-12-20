e37f4bc7f2e9bdcdad7a55f12dfd4ea5b82ecc68
Edward Banner <edward@Mac.lan>
2025-12-20 12:56:44 -0500

    Update vim install script

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,15 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-DOTFILES_DIR="$HOME/.dotfiles"
+mkdir -p "$HOME/.vim/autoload" && curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
 
-cd "$HOME"
-
-mkdir -p "$HOME/.vim/autoload"
-
-curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
-
-cd .vim
-
-git submodule update --init
+cd $HOME/.vim && git submodule update --init
